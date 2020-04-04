a0616a3fb4ceba8262f08b54ddd313fad01f9c43
Pawel Zelmanski <pzelmanski1@gmail.com>
2020-04-04 18:25:56 +0200

    Added a WIP version of a word-search exercise

---
+++ word-search/TODO.md
@@ -0,0 +1,4 @@
+# TODO
+
+- [ ] Change `binder` so it takes a list of grid elements and a single word, so that it returns a single value for [found / not found]
+- [ ] Make sure that `combinedListsSorted` in `binder` and then `Map.ofList` in `search` are working correctly - filtering out `None` when there is `Some`
---
+++ word-search/WordSearch.fs
@@ -0,0 +1,39 @@
+﻿module WordSearch
+
+open System
+
+let TryToGetWordsCoordinatesHorizontallyLeftToRight (line: string) (vertialIndex: int) (wordToSearch: string) =
+    match (line).IndexOf(wordToSearch) with
+    | -1 -> wordToSearch, None
+    | index -> wordToSearch, Some((index + 1, vertialIndex), (index + String.length wordToSearch, vertialIndex))
+
+let TryToGetWordsCoordinatesHorizontallyRightToLeft (line: string) (vertialIndex: int) (wordToSearch: string) =
+    let wordToSearchReversed =
+        wordToSearch.ToCharArray()
+        |> Array.rev
+        |> String
+
+    match (line).IndexOf(wordToSearchReversed) with
+    | -1 -> wordToSearch, None
+    | index -> wordToSearch, Some((index + String.length wordToSearch, vertialIndex), (index + 1, vertialIndex))
+
+let binder (wordsToSearchFor: string list) (verticalIndex: int) (gridElement: string) =
+    let list1 =
+        wordsToSearchFor |> List.map (TryToGetWordsCoordinatesHorizontallyRightToLeft gridElement (verticalIndex + 1))
+    let list2 =
+        wordsToSearchFor |> List.map (TryToGetWordsCoordinatesHorizontallyLeftToRight gridElement (verticalIndex + 1))
+    
+    let combinedLists = List.append list1 list2
+    let combinedListsSorted = combinedLists
+                              |> List.sortByDescending (fun item -> (snd item).IsSome)
+                              |> List.distinctBy fst
+    combinedListsSorted
+
+let search (grid: string list) (wordsToSearchFor: string list) =
+    let a = grid
+            |> List.mapi (binder wordsToSearchFor)
+            
+    let b = a |> List.collect id
+            
+    let c = b |> Map.ofList
+    c
